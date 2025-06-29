341ecef167d068f612f97cff9c9a827ee4d41cc6
Jolene Sitienei <sitieneijolene@gmail.com>
2025-06-29 14:35:44 +0300

    task 14 : impliment skills componet and render it

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,1 +1,54 @@
 // Create your Skills component here
+import { FaJs, FaReact, FaHtml5, FaCss3, FaGithub, FaDocker } from 'react-icons/fa';
+
+const Skills = () => {
+    const skillsArr = [
+        {
+            'name': 'Javascript',
+            'icon': FaJs
+        },
+        {
+            'name': 'HTML',
+            'icon': FaHtml5
+        },
+        {
+            'name': 'CSS',
+            'icon': FaCss3
+        },
+        {
+            'name': 'Git',
+            'icon': FaGithub
+        },
+        {
+            'name': 'Docker',
+            'icon': FaDocker
+        },
+        {
+            'name': 'ReactJS',
+            'icon': FaReact
+        }
+    ]
+
+    return(
+        <div id ='skills' className='skills'>
+            <h2 className='title'>Skills</h2>
+            <div className="skill-holder">
+                {
+                    skillsArr.map((skill, index)=>{
+                        const Icon = skill.icon;
+                        return(
+                            <i key={index} className='skill-cards'>
+                                <Icon className='skill-cards'/>
+                                <p className='skill'>
+                                    {skill.name}
+                                </p>
+                            </i>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Skills;
