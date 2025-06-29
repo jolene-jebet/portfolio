d6494eedb5133221e200d626457e82158b583e50
Jolene Sitienei <sitieneijolene@gmail.com>
2025-06-29 10:57:12 +0300

    task 2: create the header component

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+// Create your Header component here
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header-logo">
+                <a href="#app"><h3 className="header-name">Jolene Sitienei</h3></a>
+            </div>
+            <div className="header-container">
+                <ul>
+                    <div className="nav-item"><a href="#about">About</a></div>
+                    <div className="nav-item"><a href="#projects">Projects</a></div>
+                    <div className="nav-item"><a href="#skills">Skills</a></div>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
