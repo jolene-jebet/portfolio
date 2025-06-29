5cfbc9a23922ef962491f1d79bc21b765d8cc33b
Jolene Sitienei <sitieneijolene@gmail.com>
2025-06-29 15:05:06 +0300

    task 17/18 : impliment the footer section and render it

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,23 @@
 // Create your Footer component here
+import { FaTwitter, FaInstagram } from "react-icons/fa";
+
+const Footer = () => {
+    return (
+        <footer id='footer' className='footer'>
+            <div className='footer-contact-info'>
+                <h1 className='footer-heading'>Connect with me</h1>
+                <p className="footer-contact-access">Email: jolene.sitienei@strathmore.edu</p>
+                <p className="footer-contact-access">Phone: +254 111 763 954</p>
+            </div>
+            <div>
+                <h1>Social links</h1>
+                <div className="social-icons">
+                    <a href="https://www.educative.io/"><i><FaTwitter /></i></a>
+                    <a href="https://www.educative.io/"><i><FaInstagram /></i></a>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
