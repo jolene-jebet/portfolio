7fbe1a443d204d3afcf6039292bdc81f54ae9f72
Jolene Sitienei <sitieneijolene@gmail.com>
2025-06-29 12:10:14 +0300

    task 8/9 :create about component and render it

--- src/components/About.js
+++ src/components/About.js
@@ -1,1 +1,20 @@
 // Create your About component here
+import IMG from "../assets/Detective.png";
+
+const About = () =>{
+    return(
+        <div id = "about" className="about">
+            <h1 className="about-heading">About me</h1>
+            <div className = "about-info">
+                <p className = "about-desc">A text here about myself</p>
+                <div className="about-img">
+                    <div className="about-img-wrapper">
+                        <img src={IMG} alt="Detective"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
