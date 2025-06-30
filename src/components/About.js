86d5636c0475591034965b572c58121a4738a0d4
Jolene Sitienei <sitieneijolene@gmail.com>
2025-06-30 21:24:51 +0300

    adjust the css styling and personalize the portfolio

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,8 @@ const About = () =>{
         <div id = "about" className="about">
             <h1 className="about-heading">About me</h1>
             <div className = "about-info">
-                <p className = "about-desc">A text here about myself</p>
+                <p className = "about-desc">
+                    Hello! My name is Jolene Sitienei, and I&#39;m an enthusiast about the entire tech landscape. My experience spans from developing user-friendly mobile applications to tackling complex coding challenges, and I&#39;m eager to explore various domains within the tech field.</p>
                 <div className="about-img">
                     <div className="about-img-wrapper">
                         <img src={IMG} alt="Detective"/>
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,22 +1,22 @@
 // Create your Body component here
 import {FaGithub, FaLinkedin} from 'react-icons/fa';
-import Avatar from '../assets/Software.png';
+import Avatar from '../assets/myImg.JPG';
 
 const Body = () => {
     return(
-        <div id='body'>
+        <div id="body" className="body">
             <div className='body-container'>
                 <div className="body-profile">
                     <img src={Avatar} alt="Avatar" className="body-img" />
 
                     <div className="body-content">
                         <div className="body-headline">Jolene Sitienei</div>
-                        <div className="body-text">Software Engineer</div>
+                        <div className="body-text">Computer Scientist</div>
                     </div>
 
                     <div className="body-icons">
-                        <a href="https://github.com/jolene-sitienei" target="_blank" rel="noreferrer" className='icon-link'><FaGithub /></a>
-                        <a href="https://www.educative.io/" target="_blank" rel="noreferrer" className='icon-link'><FaLinkedin /></a>
+                        <a href="https://github.com/jolene-sitienei" target="_blank" rel="noreferrer" className='icon-link'><i><FaGithub /></i></a>
+                        <a href="https://www.linkedin.com/in/jolene-sitenei/" target="_blank" rel="noreferrer" className='icon-link'><i><FaLinkedin /></i></a>
                     </div>
                 </div>
             </div>
